a731b729afafba18b89d43e8e436acb8cd7ed890
vishalm0509 <vishal@datazip.io>
2025-12-17 13:25:00 +0530

    fix: resolve comments

--- worker/utils/logger/temporal.go
+++ worker/utils/logger/temporal.go
@@ -6,47 +6,6 @@ import (
 	"go.temporal.io/sdk/log"
 )
 
-// temporalLogger implements Temporal's log.Logger using rootLogger (stdout only).
-// Used for Temporal client/worker level logging.
-type temporalLogger struct{}
-
-func (l temporalLogger) Debug(msg string, keyvals ...interface{}) {
-	if len(keyvals) > 0 {
-		rootLogger.Debug().Fields(keyvals).Msg(msg)
-	} else {
-		rootLogger.Debug().Msg(msg)
-	}
-}
-
-func (l temporalLogger) Info(msg string, keyvals ...interface{}) {
-	if len(keyvals) > 0 {
-		rootLogger.Info().Fields(keyvals).Msg(msg)
-	} else {
-		rootLogger.Info().Msg(msg)
-	}
-}
-
-func (l temporalLogger) Warn(msg string, keyvals ...interface{}) {
-	if len(keyvals) > 0 {
-		rootLogger.Warn().Fields(keyvals).Msg(msg)
-	} else {
-		rootLogger.Warn().Msg(msg)
-	}
-}
-
-func (l temporalLogger) Error(msg string, keyvals ...interface{}) {
-	if len(keyvals) > 0 {
-		rootLogger.Error().Fields(keyvals).Msg(msg)
-	} else {
-		rootLogger.Error().Msg(msg)
-	}
-}
-
-// NewTemporalLogger creates a Temporal-compatible logger for client/worker level logging.
-func NewTemporalLogger() log.Logger {
-	return temporalLogger{}
-}
-
 // contextAwareLogger implements Temporal's log.Logger but writes to workflow file if available.
 type contextAwareLogger struct {
 	ctx context.Context
