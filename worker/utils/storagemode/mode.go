54508f233d41f04b71b01e3a4397a873f3a3b81e
saksham-datazip <saksham@datazip.io>
2026-09-01 11:39:19 +0530

    chore: Ai review

--- worker/utils/storagemode/mode.go
+++ worker/utils/storagemode/mode.go
@@ -3,11 +3,12 @@ package storagemode
 import (
 	"github.com/datazip-inc/olake-helm/worker/constants"
 	"github.com/spf13/viper"
+	"strings"
 )
 
 // Get returns OLAKE_STORAGE_MODE from the environment, defaulting to nfs.
 func Get() string {
-	mode := viper.GetString(constants.EnvStorageMode)
+	mode := strings.ToLower(strings.TrimSpace(viper.GetString(constants.EnvStorageMode)))
 	if mode == "" {
 		return constants.StorageModeNFS
 	}
